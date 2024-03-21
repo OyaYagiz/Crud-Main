f59ed0e308dc7f4ad04f349a18f99ce53ea905af
OyaYagiz <oyagiz93@gmail.com>
2024-03-21 22:21:14 +0300

    edit, add, delete, clear list features have been added

--- main.js
+++ main.js
@@ -3,14 +3,34 @@ const form = document.querySelector(".grocery-form");
 const grocery = document.getElementById("grocery");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
+const alert = document.querySelector(".alert");
+const submitBtn = document.querySelector(".submit-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
 
 //Düzenleme Seçenekleri
+let editElement;
 let editFlag = false;
+let editID ="";
 
 //! Olay İzleyicileri
 form.addEventListener("submit", addItem);
+clearBtn.addEventListener("click", clearItems);
+
 
 //Fonksiyonlar
+
+function displayAlert(text,action){
+    console.log(text,action);
+    alert.textContent = text;
+    alert.classList.add(`alert--${action}`);
+    setTimeout(() => {
+        alert.textContent=""
+        alert.classList.remove(`alert-${action}`);
+    },2000);
+}
+
+
 function addItem(e){
     e.preventDefault(); 
     const value = grocery.value; 
@@ -25,7 +45,8 @@ function addItem(e){
        element.innerHTML = `
        <p class="title">${value}</p>
        <div class="btn-container">
-           <button type="button" class="edit-btn"><i class="fa-solid fa-pen-to-square"></i>
+           <button type="button" class="edit-btn">
+           <i class="fa-solid fa-pen-to-square"></i>
            </button>
            <button type="button" class="delete-btn">
                <i class="fa-solid fa-trash-can"></i>
@@ -38,14 +59,48 @@ function addItem(e){
         const editBtn = element.querySelector(".edit-btn");
         editBtn.addEventListener("click", editItem);
 
+        //kapsayıcıya ekleme 
         list.appendChild(element);
+        displayAlert("Başarıyla Eklendi","success");
+        container.classList.add("show-container");
+        grocery.value = "";
+    }
+    else if(value !=="" && editFlag){
+        editElement.innerHTML = value;
+        displayAlert("Değer Değiştirildi", "success");
+    }else{
+
     }
 }
  
-function deleteItem(){
-    console.log("silindi");
+function deleteItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+
+    displayAlert("Öğe kladırıldı", "danger")
 }
 
-function editItem(){
-    console.log("düzenleme işlemi");
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+    console.log(items);
+    if (items.length > 0) {
+      items.forEach((item) => list.removeChild(item));
+    }
+    container.classList.remove("show-container"); // konteynarı gizle
+    displayAlert("Liste Boş", "danger");
+  }
+  
+
+function editItem(e){
+    console.log(e);
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement =  (e.currentTarget.parentElement.previousElementSibling);
+    console.log(editElement);
+    grocery.value = editElement.innerHTML;
+
+    editFlag = true;
+    console.log(element.dataset);
+    editID = element.dataset.id;
+    submitBtn.textContent="Düzenle";
 }
